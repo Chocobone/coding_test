715151fef01771dc491fda62727a7e53acc2919d
유현웅 <yooho1030@naver.com>
2025-09-18 15:13:18 +0900

    250918

---
+++ 32829.py
@@ -0,0 +1,15 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+
+runner = []
+for _ in range(n):
+    runner.append(list(map(int, sys.stdin.readline().split())))
+
+run_time = [0 * m]
+
+print(m, end=' ')
+for i in range(1, m):
+    
+
+    
