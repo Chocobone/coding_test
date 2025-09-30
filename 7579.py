fb67ed5f16fd68ac40991e54ad443aec039d3abf
유현웅 <yooho1030@naver.com>
2025-09-30 20:28:51 +0900

    250930

--- 7579.py
+++ 7579.py
@@ -1,11 +1,19 @@
 # https://www.acmicpc.net/problem/7579
 
+# dp[n][W] = max(dp[n-1][W], value + dp[n-1][W-K])
+# prob : n * W 행렬의 크기가 최대 1Trillion -> 시간복잡도 신경써야 함
+# dp[n] = max
+
 import sys
 
 n, m = map(int, sys.stdin.readline().split())
 
-weight = list(map(int, sys.stdin.readline().split()))
-value = list(map(int, sys.stdin.readline().split()))
+memory = [0] + list(map(int, sys.stdin.readline().split()))
+cost = [0] + list(map(int, sys.stdin.readline().split()))
 
-dp = [[0]*(n+1) for _ in range(n+1)]
+#dp = dict() # key = memory weight, value = cost
 
+dp = [[sum(cost)]*(m+1) for _ in range(n+1)]
+
+# for i in range(1, n+1):
+#     for j in range(1, m+1):
