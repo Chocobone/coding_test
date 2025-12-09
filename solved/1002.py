e1127d0782750d5b0c06ed2db94bea6edfbdeb2a
유현웅 <yooho1030@naver.com>
2025-12-09 19:36:33 +0900

    251209-27312 solved

--- solved/1002.py
+++ solved/1002.py
@@ -1,15 +1,17 @@
-import sys
+from sys import stdin
 import math
 
-n = int(input())
+n = int(stdin.readline())
 
 for i in range(n):
-    x1, y1, r1, x2, y2, r2 = map(float, sys.stdin.readline().split(' '))
-    distance = math.sqrt((x2-x1)**2 + (y2-y1)**2)
-
-    if(r1+r2 < distance or r1 > r2+distance or r2 > r1+distance):
+    x1, y1, r1, x2, y2, r2 = map(int, stdin.readline().split(' '))
+    dis = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+    
+    if(dis==0 and r1==r2):
+        print(-1)
+    elif(dis > r1+r2 or r1 > r2+dis or r2 > r1+dis):
         print(0)
-    elif(r1+r2 == distance or r1 == r2+distance or r2 == r1+distance):
+    elif(r1+r2 == dis or r1 == r2+dis or r2 == r1+dis):
         print(1)
     else:
         print(2)
