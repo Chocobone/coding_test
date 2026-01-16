991e51da40b218213fa93311a20010573654df0a
chocobone <yooho1030@naver.com>
2026-01-16 17:57:04 +0900

    260116-company

--- codetest_study/260116/10942.py
+++ codetest_study/260116/10942.py
@@ -1,5 +1,12 @@
 # https://www.acmicpc.net/problem/10942
-# list slicing + 대칭 비교(홀/짝 나눠서 구현)
+# sol1) list slicing + 대칭 비교(홀/짝 나눠서 구현)
+# prob: 시간 초과 걸림 - O(NM) > 1s 
+# 효율적인 방법 필요
+
+# sol2) + DP
+# 길이별 가능한 회문(펠린드롬)을 DP - bottom-up으로 구하기
+# 길이가 홀수/짝수인지에 따라 2개의 DP 만들기 - DP_odd, DP_even
+# DP_odd[2] = 
 
 import sys
 input = sys.stdin.readline
