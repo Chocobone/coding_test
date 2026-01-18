d253bf550d6f588aebc90fc6852579a3380a3d92
chocobone <yooho1030@naver.com>
2026-01-19 00:52:54 +0900

    260121 solving

--- codetest_study/260116/10942.py
+++ codetest_study/260116/10942.py
@@ -5,32 +5,61 @@
 
 # sol2) + DP
 # 길이별 가능한 회문(펠린드롬)을 DP - bottom-up으로 구하기
-# 길이가 홀수/짝수인지에 따라 2개의 DP 만들기 - DP_odd, DP_even
-# DP_odd[2] = 
+# dp[i] = 길이가 i인 펠린드롬의 시작지점
+# -> 2차원으로 관리, 길이 별로 case를 나눠 관리하기
 
 import sys
 input = sys.stdin.readline
 
 N = int(input())
 num = [0] + list(map(int, input().split()))
-M = int(input())
-for _ in range(M):
-    S, E = map(int, input().split())
-    subnum = num[S:E+1]
 
-    # if len(subnum)%2 == 1:
-    center = (len(subnum)//2)
-    is_felindrom = True
-    # print(subnum, subnum[center])
-    for i in range(center):
-        if subnum[i] != subnum[-1-i]:
-            is_felindrom = False
-            break
-    
-    if is_felindrom:
-        print(1)
-    else:
-        print(0)
+dp = [[0] * (N) for _ in range(N)]
+#dp[start][length]
+
+for i in range(1, N):
+    dp[i][i] = 1
+    dp[i][i+1] = 1 if num[i] == num[i+1] else 0
+
+for i in range(N-1, 0, -1):
+    for j in range(i+2, N+1):
+        if dp[i+1][j-1] == 1 and num[i] == num[j] :
+            dp[i][j] == 1
+
+print(dp)
+for _ in range(int(input())):
+    S, E = map(int, input().split())
+    print(dp[S][E])
+
+
+# dp[0].append(0)
+# for length in range(1, N): 
+#     for start in range(1, N-length+1):
+#         subnum = num[start:start+length+1]
+#         center = (len(subnum)//2)
+#         is_felindrom = True
+#         # print(subnum, subnum[center])
+#         for i in range(center):
+#             if subnum[i] != subnum[-1-i]:
+#                 is_felindrom = False
+#                 break
+#         if is_felindrom:
+#             dp[length+1].append(start)
+
+# print(dp)
+
+# M = int(input())
+# for _ in range(M):
+#     S, E = map(int, input().split())
+
+#     leng = E - S + 1
+        
+#     if leng == 1 or S in dp[leng]:
+#         print(1)
+#     else:
+#         print(0)
+
+
     # else:
     #     center = (len(subnum)//2)
     #     is_felindrom = True
@@ -43,4 +72,4 @@ for _ in range(M):
     #     if is_felindrom:
     #         print(1)
     #     else:
-            # print(0)
+    #         print(0)
---
+++ codetest_study/260121/33961.py
@@ -0,0 +1,62 @@
+# https://www.acmicpc.net/problem/33961
+# 1. 2*M 모양의 체스판 -> 나이숍이 한 번에 갈 수 있는 경우의 수가 4가지 밖에 없다
+# chessboard[0][N] 
+# -> chessboard[1][N-2], chessboard[1][N-1],
+#    chessboard[1][N+2], chessboard[1][N+1]ㅁㄴㅇㄹ
+
+# sol1) 나이숍 투어가 가능한 경우를 분석
+# 3의 배수 가능
+
+def three(i):# 3 + 0
+    print(1, 1+i)
+    print(2, 3+i)
+    print(1, 2+i)
+    print(2, 1+i)
+    print(1, 3+i)
+    print(2, 2+i)
+
+def four(i): # 3 + 1
+    print(1, 1+i)
+    print(2, 2+i)
+    print(1, 4+i)
+    print(2, 3+i)
+    print(1, 2+i)
+    print(2, 1+i)
+    print(1, 3+i)
+    print(2, 4+i)
+
+def five(i): # 3 + 2
+    print(1, 1+i)
+    print(2, 2+i)
+    print(1, 4+i)
+    print(2, 5+i)
+    print(1, 3+i)
+    print(2, 1+i)
+    print(1, 2+i)
+    print(2, 3+i)
+    print(1, 5+i)
+    print(2, 4+i)
+
+n = int(input())
+
+if n == 2:
+    print("NO")
+
+elif n%3 == 0:
+    print("YES")
+    for i in range(0, n, 3):
+        three(i)
+    
+elif n%3 == 1:
+    print("YES")
+    for i in range(0, n-6, 3):
+        three(i)
+    four(n-4)
+
+#else
+elif n%3 == 2:
+    print("YES")
+    for i in range(0, n-7, 3):
+        three(i)
+    four(n-5)
+    
