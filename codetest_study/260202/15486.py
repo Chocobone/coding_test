9c602fcb6009c582a4b35ec1989783cdecc683d5
chocobone <yooho1030@naver.com>
2026-02-02 16:30:41 +0900

    260202

--- codetest_study/260202/15486.py
+++ codetest_study/260202/15486.py
@@ -6,16 +6,16 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-time = []
-work = []
-for _ in range(N):
-    a, b = map(int, input().split())
-    time.append(a)
-    work.append(b)
+time = [0] * (N+1)
+work = [0] * (N+1)
+for i in range(1, N+1):
+    time[i], work[i] = map(int, input().split())
 
 dp= [0] * (N+1)
-dp[1] = work[1] if time[1] == 1 else 0
 
-for i in range(2, N+1):
-    if i + time[i] <= N+1: #당일의 일을 할 수 있으면
-        dp[i+time[i]] = max(dp[i-1]+work[i],)
+for i in range(1, N+1):
+    dp[i] = max(dp[i], dp[i-1])
+    if i + time[i] - 1 <= N: #당일의 일을 할 수 있으면
+        dp[i+time[i]-1] = max(dp[i-1]+work[i], dp[i+time[i]-1])
+
+print(dp[N])
--- codetest_study/260202/8982.py
+++ codetest_study/260202/8982.py
@@ -6,7 +6,7 @@ input = sys.stdin.readline
 
 N = int(input())
 # (x_len[i], y_len[i]), (x_len[i], y_len[i+1])
-x_len = []
+x_len = [0]
 y_len = []
 
 x_start, y_start = map(int, input().split()) # 0, 0
@@ -17,11 +17,18 @@ for i in range(N-1):
     else:
         x_len.append(x-x_start)
 
+water_total = 0
+for i in range(1, len(x_len)):
+    water_total += (x_len[i]-x_len[i-1])* y_len[i-1]
+
 K = int(input())
 for _ in range(K):
     hole_start, hole_y, hole_end, _ = map(int, input().split())
-    for i in range(x_len.index(hole_start))
+    hole_loc = x_len.index(hole_start)
+    for i in range(hole_loc):
+        if y_len
 
 
-print(x_len)
-print(y_len)
+
+# print(x_len)
+# print(y_len)
