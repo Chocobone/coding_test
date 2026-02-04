36ed1843be728b20f8d49924745848e88fdf68f7
chocobone <yooho1030@naver.com>
2026-02-04 16:43:29 +0900

    260204

---
+++ codetest_study/260204/1612/1612.py
@@ -0,0 +1,26 @@
+#https://www.acmicpc.net/problem/1612
+
+import sys
+
+N = int(sys.stdin.readline())
+
+find_num = False
+
+if N%2 == 0 or N%5 == 0:
+    pass
+# elif N != 11 and N%11 == 0:
+#     pass
+else:
+    num = 1
+    length = 1
+    while True:
+        if int(num)%N == 0:
+            find_num = True
+            break
+        num = (num * 10 + 1) % N
+        length += 1
+
+if find_num:
+    print(length)
+else:
+    print(-1)
