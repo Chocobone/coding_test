fd0b6860b11a0ce827d90ecd30a29f8f03baeb79
chocobone <yooho1030@naver.com>
2026-02-11 17:27:50 +0900

    260211

--- codetest_study/260210/1080/1080.py
+++ codetest_study/260210/1080/1080.py
@@ -1,10 +1,30 @@
+# # https://www.acmicpc.net/problem/1080
+
 import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
 Mat_A = [list(map(int, input().split)) for _ in range(N)]
-Mat_B = [list(map(int, input().split)) for _ in range(M)]
+Mat_B = [list(map(int, input().split)) for _ in range(N)]
 
-def conv(x, y):
-    for i in range(x, x+3):
-        for j in range(y, y+3):
+result = 0
+for i in range(N-2):
+    for j in range(M-2):
+        if (Mat_A[i][j] + Mat_B[i][j]) % 2 == 1:
+            for k in range(3):
+                for l in range(3):
+                    Mat_A[i+k][j+l] = (Mat_A[i+k][j+l] + 1) % 2
+                    result += 1
+
+is_flipable = True
+for i in range(N):
+    for j in range(M):
+        if Mat_A[i][j] != Mat_B[i][j]:
+            is_flipable = False
+            break
+    if not is_flipable:
+        break
+
+if is_flipable:
+    print(result)
+    
---
+++ codetest_study/260211/27448/27448.py
@@ -0,0 +1,8 @@
+# https://www.acmicpc.net/problem/27448
+
+import sys
+input = sys.stdin.readline
+
+N, M, F = map(int, input().split())
+wood = [list(input().rstrip()) for _ in range(N)]
+print(wood)
